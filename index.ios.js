ce3bf68ac3226f023cbf962a27a23ad4d9459f55
Eder <eder.ramirez87@gmail.com>
2017-06-24 19:15:02 -0500

    iOS react native navigation configuration complete

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,1 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import { AppRegistry } from 'react-native';  
-import ReactFirebase from './components/AppFirebase';
-
-AppRegistry.registerComponent('ReactFirebase', () => ReactFirebase);
+import App from './components/App';
